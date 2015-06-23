15dea1518b847b21c9a6b012e900329801478213
akephonegap <akemysgames@gmail.com>
2015-06-23 19:55:13 +0200

    material test

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
-angular.module('app', ['ionic', 'ngCordova']).config(function($stateProvider, $urlRouterProvider) {
+angular.module('app', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+
+.config(function($stateProvider, $urlRouterProvider) {
 
 	$stateProvider.state('app', {
 		url : '/',
